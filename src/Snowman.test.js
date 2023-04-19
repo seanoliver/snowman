f5dae746272e1b90e413381fd517bb0980f2d2ac
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-19 18:29:40 -0500

    tests for parts one and two

---
+++ src/Snowman.test.js
@@ -0,0 +1,37 @@
+import Snowman from "./Snowman";
+import React from "react";
+import { render, debug, fireEvent } from "@testing-library/react";
+import TEST_IMAGES from "./_testCommon";
+
+describe("tests for reaching max incorrect guesses", function () {
+  function createContainer() {
+    const { container } = render (
+      <Snowman
+        images={TEST_IMAGES}
+        words={['apple']}
+        maxWrong={2}
+      />
+    );
+    return container;
+  };
+
+  it("stops showing button area after nWrong reaches maxGuesses", function () {
+    const container = createContainer();
+    //fire off 2 btns
+    const xBtn = container.querySelector('button[value="x"]');
+    const yBtn = container.querySelector('button[value="y"]');
+    console.log("xBtn=", xBtn, "yBtn=", yBtn);
+    fireEvent.click(xBtn);
+    fireEvent.click(yBtn);
+    expect(container.querySelector('.Snowman-buttons')).not.toBeInTheDocument();
+  });
+
+  it("matches snapshot", function () {
+    const container = createContainer();
+    const xBtn = container.querySelector('button[value="x"]');
+    const yBtn = container.querySelector('button[value="y"]');
+    fireEvent.click(xBtn);
+    fireEvent.click(yBtn);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/_testCommon.js
@@ -0,0 +1,13 @@
+const TEST_IMAGES = [
+  {
+    src: "0.png"
+  },
+  {
+    src: "1.png"
+  },
+  {
+    src: "6.png"
+  }
+]
+
+export default TEST_IMAGES
